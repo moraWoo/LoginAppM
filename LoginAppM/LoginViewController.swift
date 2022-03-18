6aa1dfb9007d4e347cd4750312f19af9a9ec2409
moraWoo <pkstratech@yandex.ru>
2022-03-18 19:55:14 +0500

    Finish the project

--- LoginAppM/LoginViewController.swift
+++ LoginAppM/LoginViewController.swift
@@ -18,7 +18,45 @@ class LoginViewController: UIViewController {
     private let password = "Password"
     
     //MARK: - Navigation
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else {
+            return
+        }
+        welcomeVC.user = user
+    }
+    //MARK: IBActions
+    @IBAction func logInButtonPressed() {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+                )
+            return
+        }
+        performSegue(withIdentifier: "openWelcomeVC", sender: nil)
+    }
+    
+    @IBAction func showAuthorizationData(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Oops!", message: "Your name is \(user) 😉")
+        : showAlert(title: "Oops!", message: "Your password is \(password) 😉")
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+}
+// MARK: - Alert Controller
+extension LoginViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ок", style: .default) { _ in
+                textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
