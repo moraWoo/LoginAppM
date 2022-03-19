bc44121ea7bfcbcc319cd9fc17bcf401a23baa4d
moraWoo <pkstratech@yandex.ru>
2022-03-19 12:17:13 +0500

    Setup MVC

---
+++ LoginAppM/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  LoginAppM
+//
+//  Created by Ildar Khabibullin on 17.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
+
+
